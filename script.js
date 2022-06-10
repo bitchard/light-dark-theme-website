411ac34301485335927663b4cd17f7e637e77149
bitchard <richard@agreekhouse.com>
2022-06-10 16:25:52 -0500

    adding the fucntion of the switch event. first console log to see if it is working

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+//Switch theme Dynamically
+function switchTheme(event) {
+    console.log(event);
+}
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
