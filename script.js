e8241a1450ef75b4dca01dbad80fe6e6aec3fd8e
bitchard <richard@agreekhouse.com>
2022-06-10 16:28:50 -0500

    after checked to make sure toggle switch works then narrowing down the specific target even more.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
 //Switch theme Dynamically
 function switchTheme(event) {
-    console.log(event);
+    console.log(event.taget.checked);
 }
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
