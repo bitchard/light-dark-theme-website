11be22684caad7af87de942b41e78c4d57626a3b
bitchard <richard@agreekhouse.com>
2022-06-11 00:32:27 -0500

    took out the console log to replace with if/else statement. and used the document.documentElement which will go by the heiarchy of the html element to decide on the action or behavior it will inact. So now when the toggle switch is activated it will switch from light to dark theme.

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
 //Switch theme Dynamically
 function switchTheme(event) {
-    console.log(event.taget.checked);
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
 }
+
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
