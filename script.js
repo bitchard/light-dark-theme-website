640fc483b5204baa1331894a43dcdeac5846f309
bitchard <richard@agreekhouse.com>
2022-06-12 17:25:47 -0500

    did the dark and light styles function. to target the children of parent had to use the [1] [0] to target the specific sun and moon i needed.I had not been able to get the theme to completley switch colors like the pictures and the text well its cause in the function switchTheme at bottom I had left the O capalized in Mode so it said lightMOde instead of lightMode. such a silly mistake.

--- script.js
+++ script.js
@@ -7,20 +7,31 @@ const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
 
-//Dark or Light Images
+//Dark or Light Iages 
 function imageMode(color) {
-    image1.src = 'img/undraw_proud_coder_${color}.svg';
-    image2.src = 'img/undraw_feeling_proud_${color}.svg';
-    image3.src = 'img/undraw_conceptual_idea_${color}.svg';
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+
 }
+
 
 //Dark Mode Styles
 function darkMode() {
     nav.style.backgroundColor ='rgb(0 0 0 / 50%)';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon' );
-    imageMode(dark);
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark');
+}
+
+//Light Mode Styles
+function lightMode() {
+    nav.style.backgroundColor ='rgb(255 255 255/ 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light'); 
 }
 
 //Switch theme Dynamically
@@ -30,7 +41,7 @@ function switchTheme(event) {
         darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
-        lightMOde();
+        lightMode();
     }
 }
 
